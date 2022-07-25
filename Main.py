5137a7661382736b0edfc81f1cf6ae16e34a03d5
jorgenf <jorgen.farner@gmail.com>
2022-07-25 17:11:11 +0200

    update

--- Main.py
+++ Main.py
@@ -9,15 +9,15 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
-img = [5]
-layers = [3]
-num = [[np.random.randint(0,10) for x in range(6)]]
-inst = [1]
-w = [8]
+img = [12]
+layers = [2]
+num = [[0, 8, 0]]
+inst = [20]
+w = [4, 6]
 th = [0.7]
-p = [0.1]
+p = [0.1, 0.2]
 par = [True]
-train = [True]
+train = [False, True]
 seed = [1]
 
 params = [img, layers, num, inst, w, th, p, par, train, seed]
@@ -31,16 +31,15 @@ def do_it(img, layers, num, inst, w, th, p, par, train, seed):
     for id, i in enumerate(input):
         ij = [ij for ij in range(img**2) if np.random.random() < p]
         pop.create_input(i, j=ij, wj=w, dj=[np.random.randint(1,40) for x in range(len(ij))], trainable=train)
-
-    pop.run(1000, path='network_plots/', name=f"MEMTEST_img-{img}_layers-{layers}_num-{num}_inst-{inst}_w-{w}_th-{th}_p-{p}_par-{par}_train-{train}", record_PG=True, save_post_model=False, PG_duration=50, PG_match_th=th, save_data=False)
+    pop.run(max([max(inp) for inp in input]) + 100, path='network_plots/', name=f"MNIST_img-{img}_layers-{layers}_num-{num}_inst-{inst}_w-{w}_th-{th}_p-{p}_par-{par}_train-{train}", record_PG=True, save_post_model=False, PG_duration=50, PG_match_th=th, save_data=False)
     #for syn in pop.synapses:
         #syn.trainable = False
     #pop.run(400, record_PG=True, save_post_model=False, PG_duration=50, PG_match_th=th)
 
-do_it(5, 3, [3,4,5, 8], 1, 8, 0.7, 0.5, True, True, 1)
+#do_it(15, 2, [0, 0, 0], 20, 4, 0.7, 0.05, True, True, 1)
 
-#if __name__ == '__main__':
-#    with mp.Pool(1) as p:
-#        p.starmap(do_it, combos)
+if __name__ == '__main__':
+    with mp.Pool(1) as p:
+        p.starmap(do_it, combos)
 
 
